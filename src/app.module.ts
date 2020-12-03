8ce5ecaa1f944a78c421004b7cddc26da78ae480
Farah <kacem.farah@insat.u-carthage.tn>
2020-12-03 20:40:35 +0100

    added a little logger middleware to the transform route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
@@ -8,4 +9,12 @@ import { TodoModule } from './todo/todo.module';
   providers: [AppService],
   imports: [TodoModule],
 })
-export class AppModule {}
+//NestModule Pour pouvoir utiliser des middleware
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    //throw new Error('Method not implemented.');
+    consumer.apply(LoggerMiddleware).forRoutes('transform'); //laisser routes vides si on veut que ça soit sur toutes les routes
+    //peut aussi prendre un controlleur
+    // ou des objets du style {path:'path', method:RequestMethod.GET}, {...}
+  }
+}
